8bb0e5a45aef15e91d26324c5f3fd3da9b50f1df
CutieMath <cutie.math@protonmail.com>
2022-04-15 18:19:48 +1000

    format code

--- src/app.js
+++ src/app.js
@@ -9,56 +9,56 @@ App = {
 
     loadWeb3: async () => {
         if (typeof web3 !== 'undefined') {
-          App.web3Provider = web3.currentProvider
-          web3 = new Web3(web3.currentProvider)
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
         } else {
-          window.alert("Please connect to Metamask.")
+            window.alert("Please connect to Metamask.")
         }
         // Modern dapp browsers...
         if (window.ethereum) {
-          window.web3 = new Web3(ethereum)
-          try {
-            // Request account access if needed
-            await ethereum.enable()
-            // Acccounts now exposed
-            web3.eth.sendTransaction({/* ... */})
-          } catch (error) {
-            // User denied account access...
-          }
+            window.web3 = new Web3(ethereum)
+            try {
+                // Request account access if needed
+                await ethereum.enable()
+                // Acccounts now exposed
+                web3.eth.sendTransaction({/* ... */ })
+            } catch (error) {
+                // User denied account access...
+            }
         }
         // Legacy dapp browsers...
         else if (window.web3) {
-          App.web3Provider = web3.currentProvider
-          window.web3 = new Web3(web3.currentProvider)
-          // Acccounts always exposed
-          web3.eth.sendTransaction({/* ... */})
+            App.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+            // Acccounts always exposed
+            web3.eth.sendTransaction({/* ... */ })
         }
         // Non-dapp browsers...
         else {
-          console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
         }
-      },
+    },
 
-      loadAccount: async () => {
-          App.account = web3.eth.accounts[0];
-      },
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0];
+    },
 
-      loadContract: async () => {
+    loadContract: async () => {
         // Pull out the smart contract JSON file
         const todo = await $.getJSON('Todo.json')
         // Create a truffle contract
         //  - A JS representation of the smart contract
         App.contracts.Todo = TruffleContract(todo)
         App.contracts.Todo.setProvider(App.web3Provider)
-        
+
         // Fill the smart contract with values stored in blockchain
         App.todo = await App.contracts.Todo.deployed()
-        console.log(App.todo);
+
     },
 }
 
 $(() => {
-    $(window).load(()=>{
+    $(window).load(() => {
         App.load()
     })
 })
