f9547a3ebb6d178870db1a4647ca33ff0c1e0755
CutieMath <cutie.math@protonmail.com>
2022-04-15 15:07:37 +1000

    use public delaration for the funciton I want to use. Start steps to create list

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Todo {
     // State variable
-    uint taskCount = 0;
+    uint public taskCount = 0;
 }
 
+// 1. List tasks in the smart contract
+// 2. List tasks in the console
+// 3. List tasks in the client side application
+// 4. List tasks in the test
