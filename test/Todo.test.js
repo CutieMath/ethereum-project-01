5984e044fdd3f22533c22a290a030e01f470b1c2
CutieMath <cutie.math@protonmail.com>
2022-04-15 19:37:31 +1000

    finish tests

--- test/Todo.test.js
+++ test/Todo.test.js
@@ -15,4 +15,12 @@ contract('Todo', (accounts) => {
         assert.notEqual(address, null)
         assert.notEqual(address, undefined)
     })
+
+    it('Tasks are successfully listed', async () => {
+        const taskCount = await this.todo.taskCount()
+        const task = await this.todo.tasks(taskCount)
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.completed, false)
+        assert.equal(taskCount.toNumber(), 1)
+    })
 })
